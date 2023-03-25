24ae3707ccc399ad5b3b9b2fab1ad7c1c380a728
Yun-Xiao-Yi <1919133601@qq.com>
2023-03-25 10:54:56 +0800

    2023.03.25

---
+++ 数据库/宾馆客房管理系统/hotel.sql
@@ -0,0 +1,10 @@
+
+use  hotel 
+go
+
+Insert into Living(Rno,Cno, CleftDate ,Ccomeddate ,Ccomeddate,Camount) --添加住宿信息
+values('101','001','2022-12-10 ','2022-12-15 ');
+select * from Living order by Cno ASC;--查询住宿信息
+
+
+
